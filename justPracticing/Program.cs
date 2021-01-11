a412ebf7e28915ef44514d85e2d6c8da7f33c735
A Y Kereena <32592196+kereena@users.noreply.github.com>
2021-01-11 23:13:43 +0100

    imported files

---
+++ justPracticing/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace justPracticing
+{
+    class Program
+    {
+        // prints today's date
+        public static void Main(string[] args)
+        {
+
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
+           
+            
+            
+
+            Console.WriteLine("Enter your date of birth");
+            DateTime dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(dateOfBirth);
+            
+
+           
+            yourAge(currentDate, dateOfBirth);
+
+        }
+
+
+         public static void yourAge(DateTime cDate, DateTime dBirth)
+         {
+
+            /* TimeSpan age = cDate - dBirth ;
+             Console.WriteLine("your age is " + age.Days/365,25); */
+
+            if (cDate.Month < dBirth.Month)
+            {
+                int years = cDate.Year - dBirth.Year - 1;
+                Console.WriteLine(years);
+            }
+            else if ((cDate.Month == dBirth.Month) && (cDate.Day < dBirth.Day))
+            {
+                int years = cDate.Year - dBirth.Year - 1;
+                Console.WriteLine(years);
+
+            }
+            else { 
+                int years = cDate.Year - dBirth.Year;
+                Console.WriteLine(years);
+
+
+            }
+
+        }
+        
+
+    }
+
+}
+
---
+++ justPracticing/TodaysDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace justPracticing
+{
+    class TodaysDate
+    {
+
+        public void today()
+        {
+            DateTime todaysDT = new DateTime();
+            Console.WriteLine(todaysDT);
+        }
+}
+}
